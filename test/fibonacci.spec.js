20e26b906cf60b1ef0ecfc940d2a784a9df4e8c9
ZebGirouard <timzebgir@gmail.com>
2016-12-05 09:30:04 -0700

    Cleaning up starter code for newer/simpler version of npm

--- test/fibonacci.spec.js
+++ test/fibonacci.spec.js
@@ -1,17 +1,14 @@
 /* global define, describe, it */
+var expect = require('chai').expect;
+var fibonacci = require('../src/fibonacci');
 
-define(function (require) {
-    var expect = require('node_modules/chai/chai').expect;
-    var fibonacci = require('src/fibonacci');
-
-    describe('Testing fibonacci', function () {
-        it('should be a function', function () {
-            expect(fibonacci).to.be.a('function');
-        });
-        // TODO: implement remaining unit tests
-        it('should return 1 for the first number');
-        it('should return 1 for the second number');
-        it('should return 13 for index 6');
-        it('should return 0 for negative index');
+describe('Testing fibonacci', function () {
+    it('should be a function', function () {
+        expect(fibonacci).to.be.a('function');
     });
+    // TODO: implement remaining unit tests
+    it('should return 1 for the first number');
+    it('should return 1 for the second number');
+    it('should return 13 for index 6');
+    it('should return 0 for negative index');
 });
