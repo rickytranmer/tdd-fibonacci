7421374fff998afeeb4a2e87d1caa9bf8f3d36c6
rickytranmer <rwtranmer@gmail.com>
2018-01-03 14:47:55 -0700

    finish tests

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -6,3 +6,14 @@
      * @returns {Number} the nth element of the Fibonacci sequence.
      */
     // TODO: return definition of `fibonacci` function here
+function fibonacci(n) {
+	if (n === 0 || n === 1) {
+		return 1;
+	} else if (n > 1) {
+		return (fibonacci(n-1) + fibonacci(n-2));
+	} else {
+		return 0;
+	}
+}
+
+module.exports = fibonacci;
--- test/fibonacci.spec.js
+++ test/fibonacci.spec.js
@@ -6,9 +6,16 @@ describe('Testing fibonacci', function () {
     it('should be a function', function () {
         expect(fibonacci).to.be.a('function');
     });
-    // TODO: implement remaining unit tests
-    it('should return 1 for the first number');
-    it('should return 1 for the second number');
-    it('should return 13 for index 6');
-    it('should return 0 for negative index');
+    it('should return 1 for the first number', function() {
+    	expect(fibonacci(0)).to.equal(1);
+    });
+    it('should return 1 for the second number', function() {
+    	expect(fibonacci(1)).to.equal(1);
+    });
+    it('should return 13 for index 6', function() {
+    	expect(fibonacci(6)).to.equal(13);
+    });
+    it('should return 0 for negative index', function() {
+    	expect(fibonacci(-1)).to.equal(0);
+    });
 });
